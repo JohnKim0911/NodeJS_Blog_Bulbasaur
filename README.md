3f171032fdd1f99da4342a0ea646ee062a9967bd
John Kim <john.donggyu.kim@gmail.com>
2024-02-28 22:01:17 +0900

    Complete the project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Todo
-
-1. list of posts
--- index.js
+++ index.js
@@ -3,118 +3,105 @@ import express from "express";
 
 const app = express();
 const port = 3000;
+const allPosts = [];
+var id = 0;
 
-// Get data from ejs: use bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Set the path to public for static files
 app.use(express.static("public"));
 
-const allPosts = [];
-var id = 0;  // keeps track of id of each post. Adding 1 when creating a new post
-
-
 // --------------------------- Main page ---------------------------
+// Show the first post
 app.get("/", (req, res) => {
-    if (allPosts.length > 0) {
-        // Render main page if there's posts
-        res.render("index.ejs", { posts: allPosts });
-    } else {
-        // Otherwise render create page
-        res.redirect("/create");
-    }
+    var selectedPost = allPosts[0];
+    res.render("index.ejs", { currentPage:"home", posts: allPosts, post: selectedPost });
 });
 
+// Show selceted post
+app.get('/post', (req, res) => {
+    var currentId = req.query.id;
+    var index = getIndex(currentId);
+    var selectedPost = allPosts[index];
+    res.render("index.ejs", { currentPage:"home", posts: allPosts, post: selectedPost });
+});
 
-// --------------------------- Create page ---------------------------
+// --------------------------- Create ---------------------------
 app.get("/create", (req, res) => {
-    if (allPosts.length == 0) {
-        // when thers are no posts, show the message as well
-        res.render("create.ejs", { message: "There are no posts yet." });
-    } else {
-        res.render("create.ejs");
-    };
+    res.render("create.ejs", { currentPage:"home", posts: allPosts});
 });
 
-
-// Create backend
 app.post("/createBackend", (req, res) => {
+    var d = new Date();
+
+    var yyyy = d.getFullYear();
+    var mm = String(d.getMonth() + 1).padStart(2, '0'); //January is 0, so add 1.
+    var dd = String(d.getDate()).padStart(2, '0'); // add 0 until the length become 2
+    var today =  yyyy + '/' + mm + '/' + dd;
+
+    var hour = String(d.getHours()).padStart(2, '0');
+    var minute = String(d.getMinutes()).padStart(2, '0');
+    var time = hour + ':' + minute;
+
     var newPost = {
         id : ++id,
         title: req.body["title"],
+        date: today,
+        time: time,
         content: req.body["content"],
     };
     allPosts.push(newPost);
 
-    res.redirect("/");
+    res.redirect("/post?id=" + id);
 });
 
-
-// --------------------------- Edit page ---------------------------
-// Render edit page
+// --------------------------- Edit ---------------------------
 app.post("/edit", (req, res) => {
-    //  Get id from the index.ejs
     var id = parseInt(req.body["id"]);
-    var selectedPost;
+    var index = getIndex(id);
+    var selectedPost = allPosts[index];
 
-    // Get the selectedPost using the id
-    for (var i = 0; i < allPosts.length; i++) {
-        if (allPosts[i].id == id) {
-            // Get the index of the post
-            var index = allPosts.indexOf(allPosts[i]);
-            selectedPost = allPosts[index];
-            break;
-        };
-    };
-
-    // Render edit page
-    res.render("edit.ejs", { post: selectedPost });
+    res.render("edit.ejs", { currentPage:"home", posts: allPosts, post: selectedPost });
 });
 
-
-// Edit the post in the back. Redirect to the main page.
 app.post("/editBackend", (req, res) => {
-    //  Get id from the index.ejs
     var id = parseInt(req.body["id"]);
+    var index = getIndex(id);
 
-    // Get the selectedPost using the id
-    for (var i = 0; i < allPosts.length; i++) {
-        if (allPosts[i].id == id) {
-            // Get the index of the post
-            var index = allPosts.indexOf(allPosts[i]);
+    // Update existing post
+    allPosts[index].title = req.body["title"];
+    allPosts[index].content = req.body["content"];
 
-            // Update existing post
-            allPosts[index].title = req.body["title"];
-            allPosts[index].content = req.body["content"];
-        };
-    };
-
-    res.redirect("/");
+    res.redirect("/post?id=" + id);
 });
 
-// --------------------------- Delete post ---------------------------
-// Delete post in the back-end. Rediect to the main page
+// --------------------------- Delete ---------------------------
 app.post("/deleteBackend", (req, res) => {
-    // Get id from the index.ejs
     var id = parseInt(req.body["id"]);
-    
-    // Find the post with the id above
-    for (var i = 0; i < allPosts.length; i++) {
-        if (allPosts[i].id == id) {
-            // Get the index of the post
-            var index = allPosts.indexOf(allPosts[i]);
-            
-            // Delete the post using the index
-            allPosts.splice(index, 1);
-            break;
-        };
-    };
+    var index = getIndex(id);
+    allPosts.splice(index, 1);
 
-    // Redirect to the main page
     res.redirect("/");
 });
 
+// --------------------------- About ---------------------------
+app.get("/about", (req, res) => {
+    res.render("about.ejs", { currentPage:"about" });
+});
 
+// --------------------------- Album ---------------------------
+app.get("/album", (req, res) => {
+    res.render("album.ejs", { currentPage:"album" });
+});
+
+// --------------------------- Others ---------------------------
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+
+function getIndex(id) {
+    for (var i = 0; i < allPosts.length; i++) {
+        if (allPosts[i].id == id) {
+            var index = allPosts.indexOf(allPosts[i]);
+            return index;
+        }
+    }
+}
